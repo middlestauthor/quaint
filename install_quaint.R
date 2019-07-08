6ad13bcc6e79d1d7a7c6cc22f9861243aba2c6ea
Em <emjosephs@gmail.com>
2019-07-08 10:40:59 -0400

    cond qpc function

--- install_quaint.R
+++ install_quaint.R
@@ -1,5 +1,14 @@
-library('devtools')
-library('roxygen2')
+packages_needed <- c("devtools","roxygen2")
+
+for (i in 1:length(packages_needed)){
+  if(!(packages_needed[i] %in% installed.packages())){install.packages(packages_needed[i])}
+}
+
+for (i in 1:length(packages_needed)){
+  library( packages_needed[i], character.only = TRUE)
+}
+
+
 document()
 setwd('../')
 install('quaint')
