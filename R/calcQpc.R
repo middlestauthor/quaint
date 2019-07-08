a8cef90966bd22df0b755344b513403dce3fc840
Em <emjosephs@gmail.com>
2019-07-08 12:43:33 -0400

    updated calcQpc

--- R/calcQpc.R
+++ R/calcQpc.R
@@ -4,23 +4,18 @@
 #' @param myZ vector of traits. Not normalized yet.
 #' @param myU matrix of eigenvectors of the kinship matrix (each column is an eigenvector)
 #' @param myLambdas vector of eigenvalues of the kinship matrix 
-#' @param myPCcutoff a value that determines how many PCs you want to look at. For example, 0.5 would mean that you would look at the first set of PCs that explain 0.5 of the variation. The default here is 0.3
-#' @param tailCutoff is there if you don't want to use the last PCs to estimate Va because of excess noise. The default value is 0.9, which means that you're not using the last 0.1 of your PCs. Set to 1 if you want to use all PCs
-#' @param vapcs is the number of pcs used to estimate Va. Default is 50.
+#' @param myM the range of PCs you want to test for selection 
+#' @param myL the range of PCs used to estimate Va
 #' @export
 #' @examples
 #' calcQpc()
 
-calcQpc <- function(myZ, myU, myLambdas, myPCcutoff = 0.3, tailCutoff = 0.9, vapcs = 50){
-  myTailCutoff = round(tailCutoff*length(myLambdas)) #picks the end of the set of pcs used to calculate va
-  pcm = which(sapply(1:length(myLambdas), function(x){sum(myLambdas[1:x])/sum(myLambdas)}) > myPCcutoff)[1] #the number of pcs tested
-  
+calcQpc <- function(myZ, myU, myLambdas, myL, myM){
   myZ = myZ[1:dim(myU)[1]] - mean(myZ) #mean center phenotypes
-  myCm = (myZ %*% myU)/sqrt(myLambdas) #project + standardize by the eigenvalues
-  myQm = sapply(1:pcm, function(n){
-    var0(myCm[n])/var0(myCm[(myTailCutoff-vapcs):myTailCutoff])
-  })  #test for selection
-  myPs = sapply(1:pcm, function(x){pf(myQm[x], 1, vapcs, lower.tail=F)}) #get a pvalue
+  myCmM = (myZ %*% myU[,myM])/sqrt(myLambdas[myM]) #project + standardize by the eigenvalues for testing for selection
+  myCmL = (myZ %*% myU[,myL])/sqrt(myLambdas[myL]) #project + standardize by the eigenvalues for estimating Va
+  myQm = sapply(myM, function(n){var0(myCmM[n])/var0(myCmL) })  #test for selection
+  myPs = sapply(1:pcm, function(x){pf(myQm[x], 1, length(myL), lower.tail=F)}) #get a pvalue
   retdf = list(cm = myCm, qm = myQm, pvals = myPs)
   return(retdf)
   }
