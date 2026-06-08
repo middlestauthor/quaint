6ed9d1094c49c46014aa08406e54a27a4f1fc570
Husain Agha <middlestauthor@gmail.com>
2026-06-08 12:28:19 -0500

    Update calc_cond_qpc.R

--- R/calc_cond_qpc.R
+++ R/calc_cond_qpc.R
@@ -11,14 +11,14 @@
 #' calc_cond_qpc()
 
 calc_cond_qpc = function(focal_trait, cor_trait, eigen_k, test_pcs, var_pcs){
-  focal_centered = (focal_trait[-length(focal_trait)] - mean(focal_trait)) %*% eigen_k$vectors/sqrt(eigen_k$values)
-  cor_centered = (cor_trait[-length(cor_trait)] - mean(cor_trait)) %*% eigen_k$vectors/sqrt(eigen_k$values)
+  focal_centered = (focal_trait[1:dim(eigen_k$vectors)[1]] - mean(focal_trait)) %*% eigen_k$vectors/sqrt(eigen_k$values)
+  cor_centered = (cor_trait[1:dim(eigen_k$vectors)[1]] - mean(cor_trait)) %*% eigen_k$vectors/sqrt(eigen_k$values)
   cov_a = sum(focal_centered[var_pcs] * cor_centered[var_pcs])/length(var_pcs)
   cor_va = (sum(cor_centered[var_pcs]^2))/length(var_pcs)
   focal_va = sum(focal_centered[var_pcs]^2)/length(var_pcs)
-  mu_cond = mean(focal_trait[-length(focal_trait)]) + (cov_a/cor_va) * (cor_trait[-length(cor_trait)] - mean(cor_trait))
+  mu_cond = mean(focal_trait[1:dim(eigen_k$vectors)[1]]) + (cov_a/cor_va) * (cor_trait[1:dim(eigen_k$vectors)[1]] - mean(cor_trait))
   cond_va = focal_va - (cov_a^2)/cor_va
-  myQ = ((focal_trait[-length(focal_trait)] - mu_cond) %*% eigen_k$vectors[, test_pcs])/sqrt(eigen_k$values[test_pcs] * cond_va)
+  myQ = ((focal_trait[1:dim(eigen_k$vectors)[1]] - mu_cond) %*% eigen_k$vectors[, test_pcs])/sqrt(eigen_k$values[test_pcs] * cond_va)
   p_values = 1-pnorm(abs(myQ), mean=0, sd=1)
   return(list(myQ = myQ, mu_cond = mu_cond, cond_va = cond_va, pvals = p_values))
 }
